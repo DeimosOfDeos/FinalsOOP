c463410974d7a2631897db83d9eef153bd2194e6
DeimosOfDeos <149517524+DeimosOfDeos@users.noreply.github.com>
2023-12-07 04:03:25 +0800

    Add files via upload

---
+++ FinalsOOP/FinalsOOP/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalsOOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SignIn signin = new SignIn();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var Form2 = new Form2())
+            {
+                // Set any properties or pass data to the VideosForm if needed
+
+                // Show the VideosForm as a dialog
+                Form2.ShowDialog(this);
+            }
+        }
+
+        
+    }
+}
